114627173101cbab3cf974e8052b08db582af700
Parsifal93 <vadimstratiienko@gmail.com>
2022-02-01 00:32:00 +0200

    nesting

--- src/App.js
+++ src/App.js
@@ -1,46 +1,44 @@
-import { useState } from "react";
-import ToDo from "./ToDo";
-import ToDoForm from "./ToDoForm";
-import "./App.css";
+import { useState } from 'react'
+import ToDo from './ToDo'
+import ToDoForm from './ToDoForm'
+import './App.css' 
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([])
 
   const addTask = (userInput) => {
-    if (userInput) {
+    if(userInput) {
       const newItem = {
-        id: Math.random().toString(36).substr(2, 9),
+        id: Math.random().toString(36).substr(2,9),
         task: userInput,
-<<<<<<< HEAD
         complete: false,
-        subtask:null
       }
       setTodos([...todos, newItem])
-=======
-        complete: false
-      };
-      setTodos([...todos, newItem]);
->>>>>>> 64d60df0a197ee3f719c4b47b908c60ab3891fb8
     }
-  };
+  }
 
   const removeTask = (id) => {
-    setTodos([...todos.filter((todo) => todo.id !== id)]);
-  };
+    setTodos([...todos.filter((todo) => todo.id !== id)])
+  }
 
   const handleToggle = (id) => {
     setTodos([
-      ...todos.map((todo) =>
-        todo.id === id ? { ...todo, complete: !todo.complete } : { ...todo }
+      ...todos.map((todo) => 
+        todo.id === id ? { ...todo, complete: !todo.complete } : {...todo }
       )
-    ]);
-  };
-  const swapUp = () => {
-    setTodos([...todos.concat(...todos.splice(0, 1))]);
-  };
-  const swapDown = () => {
-    setTodos([...todos.concat(...todos.splice(0, todos.length - 1))]);
-  };
+    ])
+  }
+  const swapUp = () =>{
+    setTodos(([...todos.concat(...todos.splice(0,1))]));
+   
+   console.log(todos)
+    }
+    const swapDown = () =>{
+      setTodos(([...todos.concat(...todos.splice(0,todos.length-1))]));
+     
+     console.log(todos)
+      }
+
 
   return (
     <div className="App">
@@ -53,8 +51,8 @@ function App() {
             removeTask={removeTask}
             swapUp={swapUp}
             swapDown={swapDown}
-          />
-        );
+            />)
+            
       })}
       <ToDoForm addTask={addTask} />
     </div>
